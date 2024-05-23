c0c0b0a02be40cd79c2240294be6a4692b0634cd
Daniel Feinstat <danielfeinstat@Daniels-MBP-2.attlocal.net>
2024-05-22 20:13:09 -0700

    heroku commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,17 @@ import  express  from 'express';
 import cors from 'cors';
 import notesRoutes from './routes/notesRoutes';
 
-
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use('/api', notesRoutes);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '../../client/build/index.html'),
+    function(err: Error) {
+        if(err) {
+            res.status(500).send(err);
+        }
+    }
+});
 
 app.listen(PORT, ()=>{console.log(`Server is running on port ${PORT}`)});
